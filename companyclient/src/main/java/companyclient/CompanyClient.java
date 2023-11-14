9f794dbd896e4bfb2602fde244a84f565c221757
HariniVavilipalli <harini.vavilipalli0@gmail.com>
2023-11-14 12:02:16 +0530

    spring project

---
+++ companyclient/src/main/java/companyclient/CompanyClient.java
@@ -0,0 +1,11 @@
+package companyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyClient {
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyClient.class, args);
+	}
+}
